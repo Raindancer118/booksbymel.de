71239fc46917165d9831c11810e40448d8852d3a
Raindancer118 <tom560stieh@gmail.com>
2025-10-28 12:00:57 +0100

    Add Google Analytics gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Script id="theme-script" strategy="beforeInteractive">
           {themeScript}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MN1HKKFFKX"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MN1HKKFFKX');
+          `}
+        </Script>
         {children}
       </body>
     </html>
